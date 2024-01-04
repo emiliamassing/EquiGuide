dcc87a769ea20bfcfdc185606cb05e9293280dd4
Emilia <emiliamassings@gmail.com>
2024-01-04 17:09:53 +0100

    Added connection to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,27 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import path from 'path';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import connection from './routes/conn';
+import { QueryError } from 'mysql2';
 
 const app = express();
+
+connection.connect(function(err: QueryError | null) {
+    if(err) {
+        console.log('Error', err);
+    }
+
+    console.log('Connected to database');
+});
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded( {extended: false} ));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Application works');
