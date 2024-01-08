1d81de128043b9f11c2189b4ca8ca65080230128
Emilia <emiliamassings@gmail.com>
2024-01-08 16:47:58 +0100

    ➖Uninstalled crypto-js, ➕installed bcrypt for safer password hashing

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import connection from './conn';
-import { QueryError } from 'mysql2';
-import CryptoJs from 'crypto-js';
+import { QueryError, ResultSetHeader } from 'mysql2';
+
 const userRouter: Router = express.Router();
 
 userRouter.get('/', function(req: Request, res: Response) {
@@ -10,7 +10,7 @@ userRouter.get('/', function(req: Request, res: Response) {
             console.log('Error: ', err);
         };
 
-        connection.query('SELECT * FROM users', function(err: QueryError | null, result) {
+        connection.query('SELECT * FROM users', function(err: QueryError | null, result: ResultSetHeader) {
             if(err) {
                 console.log('Error', err);
             };
@@ -30,12 +30,12 @@ userRouter.post('/add', function(req: Request, res: Response) {
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             email: req.body.email,
-            password: CryptoJs.SHA3(req.body.password).toString()
+            password: req.body.password
         };
 
         let sql: string = `INSERT INTO users (first_name, last_name, email, password) VALUES('${newUser.first_name}', '${newUser.last_name}', '${newUser.email}', '${newUser.password}')`;
 
-        connection.query(sql, function(err: QueryError | null, result) {
+        connection.query(sql, function(err: QueryError | null, result: ResultSetHeader) {
             if(err) {
                 console.log('Error: ', err);
             };
@@ -43,6 +43,16 @@ userRouter.post('/add', function(req: Request, res: Response) {
             console.log('New user', result);
             res.status(200).json(result);
         });
+    });
+});
+
+userRouter.post('/login', function(req: Request, res: Response) {
+    connection.connect(function(err: QueryError | null) {
+        if(err) {
+            console.log('Error: ', err);
+        };
+
+       
     });
 });
 
