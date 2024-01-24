2c6a295c4c2a3b6b0a98b5e4b939e29b92b13d9d
Emilia <emiliamassings@gmail.com>
2024-01-24 16:43:07 +0100

    💄 Added styling for menu

--- client/src/components/menu/Menu.tsx
+++ client/src/components/menu/Menu.tsx
@@ -8,26 +8,31 @@ export function Menu() {
                     <li>
                         <NavLink to={'/app/home'}>
                             <span className="material-symbols-outlined">home</span>
+                            <span className="navText">Hem</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to={'/app/calendar'}>
                             <span className="material-symbols-outlined">calendar_month</span>
+                            <span className="navText">Kalender</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to={'/app/addRide'}>
                             <span className="material-symbols-outlined">add_circle</span>
+                            <span className="navText">Planera ridpass</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to={'/app/explore'}>
                             <span className="material-symbols-outlined">search</span>
+                            <span className="navText">Utforska</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to={'/app/profile'}>
                             <span className="material-symbols-outlined">person</span>
+                            <span className="navText">Profil</span>
                         </NavLink>
                     </li>
                 </ul>
