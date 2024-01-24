71093c0001b57a49d485709c738fbf575ebf2d6f
Emilia <emiliamassings@gmail.com>
2024-01-24 19:01:19 +0100

    ✨Added endpoint to get all rides and add rides

--- server/src/routes/ridingSessions.ts
+++ server/src/routes/ridingSessions.ts
@@ -1,9 +1,97 @@
 import express, { Request, Response, Router } from 'express';
+import connection from './conn';
+import { QueryError, ResultSetHeader } from 'mysql2';
 
 const ridingSessionRouter: Router = express.Router();
 
 ridingSessionRouter.get('/', function(req: Request, res: Response) {
-    res.send('ridingSessions');
+    connection.connect(function(err: QueryError | null) {
+        if(err) {
+            console.log('Error connecting to database');
+            return res.status(500).json({ error: 'Database connection error' });
+        };
+
+        let sql: string = `SELECT * FROM rides`;
+
+        connection.query(sql, function(err: QueryError | null, result: ResultSetHeader) {
+            if(err) {
+                console.log('Error selecting rides');
+                return res.status(500).json({ error: 'Error selecting horses' });
+            }
+
+            console.log('All rides:', result);
+            res.send(result);
+        });
+    });
+});
+
+ridingSessionRouter.post('/add', function(req: Request, res: Response) {
+    let newRidingSession = {
+        title: req.body.title,
+        date: req.body.date,
+        duration: req.body.duration,
+        discipline: req.body.discipline,
+        notes: req.body.notes,
+        rating: req.body.rating
+    };
+
+    let userId = {
+        id: req.body.userId
+    };
+
+    let horseId = {
+        id: req.body.horseId
+    }
+
+    connection.connect(function(err: QueryError | null) {
+        if(err) {
+            console.log('Error connecting to database', err);
+            return res.status(500).json({ error: 'Database connection error' });
+        };
+
+        let sql: string = `
+        INSERT INTO rides 
+        (title, date, duration, discipline, notes, rating)
+        VALUES
+        ('${newRidingSession.title}', '${newRidingSession.date}', '${newRidingSession.duration}', '${newRidingSession.discipline}', '${newRidingSession.notes}', '${newRidingSession.rating}')`;
+
+        connection.query(sql, function(err: QueryError | null, result: ResultSetHeader) {
+            if(err) {
+                console.log('Error inserting ride', err);
+                return res.status(500).json({ error: 'Error inserting ride' });
+            }
+
+            const rideId = result.insertId;
+            const userJunctionSql = `
+            INSERT INTO rides_users
+            (ride_id, user_id) 
+            VALUES 
+            ('${rideId}', '${userId.id}')`;
+
+            const horseJunctionSql = `
+            INSERT INTO rides_horses
+            (ride_id, horse_id)
+            VALUES
+            ('${rideId}', '${horseId.id}')`;
+
+            connection.query(userJunctionSql, function(err: QueryError | null, result: ResultSetHeader) {
+                if(err) {
+                    console.log('Error inserting into junction table, user', err);
+                    return res.status(500).json({ error: 'Error inserting into junction table, user'});
+                }
+
+                connection.query(horseJunctionSql, function(err: QueryError | null, result: ResultSetHeader) {
+                    if(err) {
+                        console.log('Error inserting into junction table, horse', err);
+                        return res.status(500).json({ error: 'Error inserting into junction table, horse'});
+                    }
+    
+                    console.log('Created riding session and instered into junction tables');
+                    res.status(200).json(result)
+                });
+            });
+        });
+    });
 });
 
 export default ridingSessionRouter;
