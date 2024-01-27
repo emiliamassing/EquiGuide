2c46a17e07f42f4fd95ce38913234ffbf8bccc28
Emilia <emiliamassings@gmail.com>
2024-01-27 15:31:26 +0100

    🎨 Updated endpoint for selecting all rides

--- server/src/routes/ridingSessions.ts
+++ server/src/routes/ridingSessions.ts
@@ -11,7 +11,7 @@ ridingSessionRouter.get('/', function(req: Request, res: Response) {
             return res.status(500).json({ error: 'Database connection error' });
         };
 
-        let sql: string = `SELECT * FROM rides`;
+        let sql: string = `SELECT * FROM rides WHERE is_deleted=0`;
 
         connection.query(sql, function(err: QueryError | null, result: ResultSetHeader) {
             if(err) {
