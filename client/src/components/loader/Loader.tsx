fb6876cbf3d944e921dded6deb3f22723fa63368
Emilia <emiliamassings@gmail.com>
2024-02-01 16:50:45 +0100

    ✨ Added loader component

---
+++ client/src/components/loader/Loader.tsx
@@ -0,0 +1,9 @@
+import { PulseLoader } from "react-spinners";
+
+export function Loader() {
+    return(
+        <>
+            <PulseLoader color="#0A3B2C" speedMultiplier={0.7}></PulseLoader>
+        </>
+    )
+}
