e3194fac2035507c08aecd11ef751264fe27150c
Emilia <emiliamassings@gmail.com>
2024-10-02 15:43:57 +0200

    ✨ Added endpoint to change user data, flexible to what data is changed

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -81,6 +81,89 @@ userRouter.post('/add', function(req: Request, res: Response) {
     });
 });
 
+userRouter.post('/edit/:id', function(req:Request, res: Response) {
+    let updatedUserData = {
+        first_name: req.body.first_name ? connection.escape(req.body.first_name) : null,
+        last_name: req.body.last_name ? connection.escape(req.body.last_name) : null,
+        email: req.body.email ? connection.escape(req.body.email) : null,
+        password: req.body.password ? connection.escape(req.body.password) : null
+    }
+
+    let userId = req.params.id;
+
+    connection.connect(function(err: QueryError | null) {
+        if(err) {
+            console.log('Error connecting to database');
+            return res.status(500).json({ error: 'Database connection error' });
+        };
+
+        if(updatedUserData.email) {
+            let checkUniqueEmailSql: string = `SELECT * FROM users WHERE email = ${updatedUserData.email}`;
+
+            connection.query(checkUniqueEmailSql, function(err: QueryError | null, result: RowDataPacket[]) {
+                if(err) {
+                    console.log('Error checking email uniqueness:', err);
+                    return res.status(500).json({ error: 'Error checking email uniqueness' });
+                }
+    
+                //Return error is email exists
+                if( result && result.length > 0) {
+                    return res.status(400).json({ error: 'Error, Email already exists' });
+                }
+
+                updateUser();
+            });
+        } else {
+            updateUser();
+        }
+
+        function updateUser() {
+            if(updatedUserData.password) {
+                bcrypt.hash(updatedUserData.password, 10, function(err, hash) {
+                    if(err) {
+                        console.log('Error hashing password:', err);
+                        return res.status(500).json({ error: 'Error hashing password' });
+                    }
+
+                    updatedUserData.password = `'${hash}'`;
+
+                    updateDatabase();
+                });
+            } else {
+                updateDatabase();
+            }
+        }
+
+        function updateDatabase() {
+            let updatedFields = [];
+            if (updatedUserData.first_name) updatedFields.push(`first_name = ${updatedUserData.first_name}`);
+            if (updatedUserData.last_name) updatedFields.push(`last_name = ${updatedUserData.last_name}`);
+            if (updatedUserData.email) updatedFields.push(`email = ${updatedUserData.email}`);
+            if (updatedUserData.password) updatedFields.push(`password = ${updatedUserData.password}`);
+
+            if(updatedFields.length === 0) {
+                return res.status(400).json({ error: 'No fields to update' });
+            }
+
+            let sql = `
+                UPDATE users
+                SET ${updatedFields.join(', ')}
+                WHERE id=${userId}
+            `;
+
+            connection.query(sql, function(err: QueryError | null, result: ResultSetHeader) {
+                if (err) {
+                    console.log('Error updating user:', err);
+                    return res.status(500).json({ error: 'Error updating user' });
+                }
+
+                console.log('User updated successfully:', result);
+                res.status(200).json(result);
+            });
+        }
+    })
+});
+
 userRouter.post('/login', function(req: Request, res: Response) {
     let userLogin = {
         email: connection.escape(req.body.email),
