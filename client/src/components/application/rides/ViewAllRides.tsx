1717d3be3f96af1e8233f4befbf9bc5797184bb6
Emilia <emiliamassings@gmail.com>
2024-10-08 10:32:32 +0200

    ✨ Added interface for showing all rides

--- client/src/components/application/rides/ViewAllRides.tsx
+++ client/src/components/application/rides/ViewAllRides.tsx
@@ -1,12 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useGetRidesByUserId } from "../../../hooks/useGetRidesbyUserId";
 import { isAuthenticated } from "../../../services/tokenService";
 import { NotAuthenticated } from "../../error/NotAuthenticated";
+import { ShowLoader } from "../../loader/ShowLoader";
 import { AppHeading } from "../layouts/AppHeading";
+import { IRideData } from "../../../models/IRideData";
+import { useContext } from "react";
+import { RideContext } from "../../../contexts/RideContext";
+import { RideActionTypes } from "../../../types/ActionTypes";
 
 export function ViewAllRides() {
+    const { rides, ridesIsLoading } = useGetRidesByUserId();
+    const { rideDispatch } = useContext(RideContext);
+    const navigate = useNavigate();
+
+    function directToEditor(ride: IRideData) {
+        rideDispatch({ type: RideActionTypes.PASS_DATA, payload: JSON.stringify(ride) });
+        navigate('/app/editor');
+    }
+
+    function directToViewRide(ride: IRideData) {
+        rideDispatch({ type: RideActionTypes.PASS_DATA, payload: JSON.stringify(ride) });
+        navigate('/app/viewRide')
+    }
+
     return(
         isAuthenticated() ? (
+            ridesIsLoading ?
+                <ShowLoader></ShowLoader>
+            :
             <div className="container">
                 <AppHeading title="Alla ridpass"></AppHeading>
+                <div className="innerContainer">
+                    <div className="allRidesWrapper">
+                        {rides.map((ride) => (
+                            <div className="viewAllRidesContainer" key={ride.id}>
+                                <h3>{ride.title}, {ride.horse_name}</h3>
+                                <p>{new Date(ride.date).toLocaleDateString()}</p>
+                                <div className="ridesButtonContainer">
+                                    <button className="secondaryButton" onClick={() => directToEditor(ride)}>Redigera</button>
+                                    <button className="primaryButton" onClick={() => directToViewRide(ride)}>Visa</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         ) : (
             <NotAuthenticated></NotAuthenticated>
