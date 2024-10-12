a835e6c4b90ebdeae440424803b7d38f1f7c74fb
Emilia <emiliamassings@gmail.com>
2024-10-12 20:25:52 +0200

    -

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,6 @@ import horseRouter from './routes/horses';
 import ridingSessionRouter from './routes/ridingSessions';
 
 const app = express();
-const PORT = Number(process.env.DB_PORT) || 3000;
 
 connection.connect(function(err: QueryError | null) {
     if(err) {
@@ -40,6 +39,6 @@ app.use('/users', userRouter);
 app.use('/horses', horseRouter);
 app.use('/ridingSessions', ridingSessionRouter);
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log('App started on port 3000');
 });
