9d1ebb80cdf9a38e2f9ab58035d2af7506d75775
Emilia <emiliamassings@gmail.com>
2024-10-12 19:40:24 +0200

    🚧 Adjusted db settings

--- server/src/routes/conn.ts
+++ server/src/routes/conn.ts
@@ -4,12 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 25060,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: true,
+    ssl: {
+        rejectUnauthorized: true,
+    }
 });
 
 export default connection;
